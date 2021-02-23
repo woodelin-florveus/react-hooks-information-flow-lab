002dcc5dd24636f69e8c9225618efc7bca0e5b39
woodelin-florveus <woodelinflorveus@gmail.com>
2021-02-23 18:47:01 -0500

    commit tree 5b60b1c928a8d9c3c810ff4081bf5f3712013f45

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header"
+
 
 function App() {
   const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setColor] = useState(false)
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
-  }
+  // console.log(itemData)
+
+  function handleDarkModeClick(){
+    setColor((isDarkMode) => !isDarkMode)
+}
 
   return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+    <div className={"App " + (isDarkMode ? "light" : "dark")}>
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick}/>
       <ShoppingList items={items} />
+  
     </div>
   );
 }
+
+// Create a Filter component by refactoring the <select> element out of the 
+// ShoppingList component. Changing the selection in the dropdown should still 
+// change which items are displayed in the shopping list.
+// You will need to pass a callback function as a prop called 
+// onCategoryChange to the Filter component in order for the test to pass.
+
+
 
 export default App;
